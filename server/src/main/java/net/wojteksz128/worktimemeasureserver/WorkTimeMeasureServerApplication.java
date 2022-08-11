e7043308cd6c1682b3eaaeb0ee213c4a17842e6e
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2022-08-11 16:59:29 +0200

    Init server implementation using Spring Initializr

---
+++ server/src/main/java/net/wojteksz128/worktimemeasureserver/WorkTimeMeasureServerApplication.java
@@ -0,0 +1,13 @@
+package net.wojteksz128.worktimemeasureserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTimeMeasureServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTimeMeasureServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/net/wojteksz128/worktimemeasureserver/WorkTimeMeasureServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.wojteksz128.worktimemeasureserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTimeMeasureServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
