f09c87ec8ef0dbc345b7cf18107d3934d4f2ae09
Wojciech Szczepaniak <szczepaniak.brachowice@gmail.com>
2022-08-11 17:06:23 +0200

    Implement simple page with Greetings for user

---
+++ server/src/main/java/net/wojteksz128/worktimemeasureserver/greeting/Greeting.java
@@ -0,0 +1,20 @@
+package net.wojteksz128.worktimemeasureserver.greeting;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
